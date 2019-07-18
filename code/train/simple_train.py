357e02a68db6f779006ba85361b8bb835e8aefd2
takuro1221 <takuro1221.it@gmail.com>
2019-07-18 13:02:03 +0900

    update code/train/simple_train.py

--- code/train/simple_train.py
+++ code/train/simple_train.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import os
@@ -10,40 +13,51 @@ from multiprocessing import Process, Pipe
 import time
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 import lightgbm as lgb
 sys.path.append('../preprocess/')
 from main_merge import merge_features
 from file_read import get_df
-from first_train import train
-
-if len(sys.argv)==1:
-    FIRST_TRAIN = True
-else:
-    FIRST_TRAIN = False
 
 df_list = get_df(['train','structures'])
 train_df = df_list[0]
 structure_df = df_list[1]
 
 X, names = merge_features()
-y = train_df.values[:,-1]
+y = train_df.values[:,-1].astype(np.float32)
 
-def Ad_split(X,y,names):
-    X_df = pd.concat([pd.DataFrame(X, columns=names),
-                      pd.DataFrame(y, columns=['target'])],axis=1)
-    X_df = X_df.sample(frac=1, random_state=0)
-    first_df = X_df[:len(X_df//2)]
-    first_df = first_df.drop(['target'],axis=1)
-    second_arr = X_df.loc[:len(X_df)-len(first_df)].values
-    firstX_column = [n for n in names if 'AD_'!=n[:3]]
-    firsty_column = [n for n in names if 'AD_'==n[:3]]
-    first_X = first_df[firstX_column].values
-    first_y = first_df[firsty_column].values
-    first_X = first_X.reshape(len(first_X),len(firstX_column)).astype(np.float32)
-    first_y = first_y.reshape(len(first_y),len(firsty_column)).astype(np.float32)
-    second_X = second_arr[:,:-1].astype(np.float32)
-    second_y = y.astype(np.float32)
-    return first_X, first_y, second_X, second_y
+def Ad_split(X,y,names,AD=True):
+    
+    index = np.array(range(0,len(X)),dtype=np.int32)
+    np.random.seed(1)
+    index = np.random.permutation(index)
+    first_idx = index[:len(X)//2]
+    second_idx = index[len(X)//2:len(X)]
+    normal_idx = np.array([i for i,n in enumerate(names) if n[:3]!='AD_'])
+    
+    if AD:    
+        add_idx = np.array([i for i,n in enumerate(names) if n[:3]=='AD_'])
+
+        first_Xy = X[first_idx,:]
+        first_X = first_Xy[:,normal_idx].astype(np.float32)
+        first_X = first_X.reshape(len(first_idx),len(normal_idx))
+        first_y = first_Xy[:,add_idx].astype(np.float32)
+        first_y = first_y.reshape(len(first_idx),len(add_idx))
+
+        second_X = X[second_idx,:]
+        second_X = second_X[:,normal_idx].astype(np.float32)
+        second_X = second_X.reshape(len(second_idx),len(normal_idx))
+        second_y = y[second_idx].astype(np.float32)
+
+        return first_X, first_y, second_X, second_y, second_idx
+    else:
+        second_X = X[second_idx,:]
+        second_X = second_X[:,normal_idx].astype(np.float32)
+        second_X = second_X.reshape(len(second_idx),len(normal_idx))
+        second_y = y[second_idx].astype(np.float32)
+        
+        return second_X, second_y, second_idx
 
 def Feature_importance(fti, names):
     dic = {}
@@ -52,8 +66,9 @@ def Feature_importance(fti, names):
     for k, v in sorted(dic.items(),key=lambda x :x[1], reverse=True):
         print(str(k) + ": " + str(v))
 
-def Evaluation(y, pred):
-    types = train_df['type'].values
+def Evaluation(y, pred, index):
+    type_arr = train_df['type'].values
+    types = type_arr[index]
     dic = {'1JHC':[], '2JHH':[], '1JHN':[], '2JHN':[], '2JHC':[], '3JHH':[], '3JHC':[], '3JHN':[]}
     loss = np.abs(y - pred)
     score=0
@@ -64,24 +79,55 @@ def Evaluation(y, pred):
     score/=len(dic)
     return score
 
-first_X,first_y, second_X, second_y = Ad_split(X,y,names)
+def simple_evaluation(y, pred):
+    result = np.abs(y-pred)
+    result = np.log(result.astype(float))
+    result = np.mean(result)
+    return result
 
-if FIRST_TRAIN:
-    train(first_X, first_y, max_epoch=40)
+add_name = np.array([n for n in names if n[:3]=='AD_'])
+
+if len(add_name) != 0:
+    first_X, first_y, second_X, second_y, second_index = Ad_split(X,y,names,AD=True)
+    
+    kf = KFold(n_splits=5)
+    mse = []
+    for train_idx, test_idx in tqdm(kf.split(first_X), total=5):
+        train_X, test_X = first_X[train_idx], first_X[test_idx]
+        train_y, test_y = first_y[train_idx], first_y[test_idx]
+        vector = []
+        for i in range(train_y.shape[1]):
+            first_model = lgb.LGBMModel(objective='regression')
+            first_model.fit(train_X, train_y[:,i])
+            first_model.booster_.save_model('first_model_'+str(i)+'.txt')
+            pred = first_model.predict(test_X)
+            vector.append(mean_squared_error(test_y[:,i], pred))
+        mse.append(vector)
+    print('5-Fold score(mse):', np.mean(mse, axis=0))
+
+    add = np.zeros((len(second_X),first_y.shape[1]), dtype=np.float32)
+    for i in range(first_y.shape[1]):
+        bst = lgb.Booster(model_file='first_model_'+str(i)+'.txt')
+        add[:,i] = bst.predict(second_X)
+    second_X = np.concatenate([second_X,add],axis=1)
+
+else:
+    second_X, second_y, second_index = Ad_split(X,y,names,AD=False)
 
 kf = KFold(n_splits=10)
 score = []
 for train_idx, test_idx in tqdm(kf.split(second_X), total=10):
-    train_X, test_X = second_X[train_idx], second_X[test_idx]
+    train_X, test_X, shuffle_idx = second_X[train_idx], second_X[test_idx], second_index[test_idx]
     train_y, test_y = second_y[train_idx], second_y[test_idx]
 
     #model = RandomForestRegressor(n_estimators=100, n_jobs=-1)
     model = lgb.LGBMModel(objective='regression')
     model.fit(train_X, train_y)
     pred = model.predict(test_X)
-    score.append(Evaluation(test_y, pred))
+    score.append(Evaluation(test_y, pred, shuffle_idx))
 
 print('10-Fold score:', np.mean(score))
 print('Feature Importance')
 model.booster_.save_model('second_model.txt')
 Feature_importance(model.feature_importances_, names)
+
